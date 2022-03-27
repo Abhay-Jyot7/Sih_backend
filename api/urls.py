107411eca30640986ede4cc0f2f55a9763a4ea9a
unknown <ayush02av@gmail.com>
2022-03-27 10:44:37 +0530

    Added dynamic crop predict feature

--- api/urls.py
+++ api/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.crop_predict.as_view()),
+    path('crop/', views.crop_predict.as_view()),
 ]
--- api/views.py
+++ api/views.py
@@ -6,11 +6,7 @@ from rest_framework import status
 from django.http import HttpResponse
 
 class crop_predict(APIView):
-
-    def get(self, request):
-        from controllers.crop_predict import crop_predict
-
-        sample_crop_attributes = [
+    SAMPLE_CROP_ATTRIBUTES = [
             90,
             40,
             40,
@@ -20,10 +16,36 @@ class crop_predict(APIView):
             200
         ]
 
-        response = {
-            "status": "success",
-            "sample_crop_attributes": sample_crop_attributes,
-            "prediction": crop_predict(sample_crop_attributes)[0]
-        }
-        
-        return Response(response)
+    def get(self, request):
+        return Response({
+            'msg': 'success',
+            'sample_crop_attributes': self.SAMPLE_CROP_ATTRIBUTES
+        })
+    
+    def post(self, request):
+        from controllers.crop_predict import crop_predict
+
+        if not request.data.get('crop_attributes'):
+            return Response({
+                'msg': 'No crop attributes provided',
+                'sample_crop_attributes': self.SAMPLE_CROP_ATTRIBUTES
+            }, status=status.HTTP_400_BAD_REQUEST)
+
+        crop_attributes = request.data.get('crop_attributes')
+
+        if len(crop_attributes) != len(self.SAMPLE_CROP_ATTRIBUTES):
+            return Response({
+                'msg': 'Wrong number of crop attributes provided',
+                'sample_crop_attributes': self.SAMPLE_CROP_ATTRIBUTES
+            }, status=status.HTTP_400_BAD_REQUEST)
+
+        try:
+            return Response({
+                'msg': 'success',
+                'crop_attributes': crop_attributes,
+                'prediction': crop_predict(crop_attributes)[0]
+            })
+        except:
+            return Response({
+                'msg': 'Error while processing request',
+            }, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
